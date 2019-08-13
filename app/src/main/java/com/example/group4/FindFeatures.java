f3a086b22ad44640402280b5486c6a82960a3f3d
Padraic Cashin <pcashin@asu.edu>
2019-08-13 04:30:25 -0700

    Added print statements to the features

--- app/src/main/java/com/example/group4/FindFeatures.java
+++ app/src/main/java/com/example/group4/FindFeatures.java
@@ -1,23 +1,26 @@
 package com.example.group4;
 
+import android.util.Log;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 enum Features {
     PPEAK, NPEAK, INTERCEPT;
 }
 
 public class FindFeatures {
-    private float threashold = (float)0.5;
+    private float threshold = (float)0.5;
 
     public ArrayList<Features> tagFeatures(ArrayList<Float> values) {
         ArrayList<Features> features = new ArrayList<>();
 
         for(int i = 2; i < values.size()-1; ++i) {
-            if(((values.get(i - 1) - values.get(i)) < (-1*threashold)) &&
-                    ((values.get(i) - values.get(i + 1)) > threashold)) {
+            if(((values.get(i - 1) - values.get(i)) < (-1*threshold)) &&
+                    ((values.get(i) - values.get(i + 1)) > threshold)) {
                 features.add(Features.PPEAK);
-            } else if (((values.get(i - 1) - values.get(i)) > threashold) &&
-                    ((values.get(i) - values.get(i + 1)) < (-1*threashold))) {
+            } else if (((values.get(i - 1) - values.get(i)) > threshold) &&
+                    ((values.get(i) - values.get(i + 1)) < (-1*threshold))) {
                 features.add(Features.NPEAK);
             } else if (((values.get(i-1) < 0) && (values.get(i) > 0)) ||
                     ((values.get(i-1) > 0) && (values.get(i) < 0))) {
@@ -26,6 +29,7 @@ public class FindFeatures {
                 continue;
             }
         }
+        Log.i("Features", features.toString());
         return features;
     }
 }
